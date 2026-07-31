846cec4db0cc45105dd728fe7eacacdce11afee3
SHIVA <pals36460@gmail.com>
2026-07-31 09:50:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ if st.button("Generative Resume"):
         code=main_agent(agent,user_info)
         st.html(code,width="stretch",unsafe_allow_javascript=True)
         st.divider()
-        job_code=getjobs(agent,location,profile)
+        job_code=get_jobs(agent,location,profile)
         st.html(job_code,width="stretch",unsafe_allow_javascript=True)
 #==================display code for jobs searching==============
 # """code=get_jobs(agent)
